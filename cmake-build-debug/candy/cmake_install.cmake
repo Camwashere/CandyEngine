ed9bd1dce5e57c1dcd5916c8f0c1982aa72393e1
Camwashere <camworkmccurry@gmail.com>
2023-06-17 17:44:10 -0600

    Further submodule seperation

--- cmake-build-debug/candy/cmake_install.cmake
+++ cmake-build-debug/candy/cmake_install.cmake
@@ -49,6 +49,16 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("C:/Users/perso/CLionProjects/CandyEngineAll/cmake-build-debug/candy/core/noise/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/perso/CLionProjects/CandyEngineAll/cmake-build-debug/candy/core/collections/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("C:/Users/perso/CLionProjects/CandyEngineAll/cmake-build-debug/candy/lib/glad/cmake_install.cmake")
 endif()
 
