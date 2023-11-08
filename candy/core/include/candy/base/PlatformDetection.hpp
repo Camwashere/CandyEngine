c4814380662e7c2b314982b652539a6367e9c77f
Camwashere <camworkmccurry@gmail.com>
2023-11-08 16:56:00 -0700

    Platform detection allowed for linux

--- candy/core/include/candy/base/PlatformDetection.hpp
+++ candy/core/include/candy/base/PlatformDetection.hpp
@@ -36,7 +36,6 @@
 	#error "Android is not supported!"
 #elif defined(__linux__)
 	#define CANDY_PLATFORM_LINUX
-	#error "Linux is not supported!"
 #else
 	/* Unknown compiler/platform */
 	#error "Unknown platform!"
